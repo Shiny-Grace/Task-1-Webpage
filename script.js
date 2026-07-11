f0c3fb3c4c65c3eae8d93175baab98ca897a2ee5
Shiny-Grace <shinyjuliuskancharla@gmail.com>
2026-07-11 11:45:11 +0530

    Add files via upload
    Updated ByteBites homepage

--- script.js
+++ script.js
@@ -3,3 +3,62 @@ function welcome(){
 alert("🍔 Welcome to ByteBites!\nEnjoy your meal 😊");
 
 }
+
+const search=document.getElementById("search");
+
+search.addEventListener("keyup",function(){
+
+let value=search.value.toLowerCase();
+
+let cards=document.querySelectorAll(".card");
+
+let found=false;
+
+cards.forEach(function(card){
+
+let food=card.innerText.toLowerCase();
+
+if(food.includes(value)){
+
+card.style.display="block";
+
+found=true;
+
+}
+else{
+
+card.style.display="none";
+
+}
+
+});
+
+document.getElementById("noResult").style.display=found?"none":"block";
+
+});
+
+function filterMenu(category){
+
+let cards=document.querySelectorAll(".card");
+
+cards.forEach(function(card){
+
+if(category=="all"){
+
+card.style.display="block";
+
+}
+else if(card.classList.contains(category)){
+
+card.style.display="block";
+
+}
+else{
+
+card.style.display="none";
+
+}
+
+});
+
+}
